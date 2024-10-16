d1738d95d134f8c6fc35139c3a159141a6f8edbd
josbancar <banacloyjcarcelen@gmail.com>
2024-10-16 19:33:30 +0200

    v2

--- ej2/recordar.php
+++ ej2/recordar.php
@@ -1,55 +1,19 @@
+<?php 
+    session_start();
 
-
-
-
-<?php
-session_start();
-
-// Si el usuario selecciona "Recordar"
-if (isset($_REQUEST['recordar'])) {
-    // Guardar el nombre de usuario y la contraseña en la sesión
-    $_SESSION['nombreUser'] = $_POST['nombre'];
-    $_SESSION['passUser'] = $_POST['pass'];
-} else {
-    // Si no se selecciona "Recordar", eliminar los datos de la sesión
-    unset($_SESSION['nombreUser']);
-    unset($_SESSION['passUser']);
-}
-
-// Mostrar mensaje de bienvenida
-if (isset($_SESSION['nombreUser'])) {
-    echo "Bienvenido, " . $_SESSION['nombreUser'];
-} else {
-    echo "Por favor, inicie sesión.";
-}
-// session_start();
-// if (isset($_POST['recordar'])){
-        // $_SESSION['nombreUser'] = $_POST['nombre'];
-        // $_SESSION['passUser'] = $_POST['pass'];
-    // }
+    if (isset($_POST['recordar'])){
+        $_SESSION['nombreUser'] = $_POST['nombre'];
+        $_SESSION['passUser'] = $_POST['pass'];
+        setcookie('user', $_SESSION['nombreUser'], time()+1000);
+        echo "Se creo la cookie.";
+    }
     // else{
     //     unset($_SESSION['nombreUser']);
     //     unset($_SESSION['passUser']);
     //     session_destroy();
     // }
-    // echo "Bienvenido " . $_SESSION['nombreUser'];
-  /*
-session_start();
-
-// Verificar si se envió el formulario
-
-
-    // Si se selecciona "Recordar", guardar en sesión
-if (isset($_REQUEST['recordar'])) {
-    $_SESSION['nombreUser'] = $_REQUEST['nombre']; // Guardar nombre en sesión
-    $_SESSION['passUser'] = $_POST['pass']; // Opcional, solo para recordar, no recomendado
-} 
-
-print_r($_SESSION);
-echo $_SESSION['nombreUser'];
-   
- else {
-    echo "Por favor, inicie sesión.";
-}
-*/
+    if (isset($_COOKIE['user'])&& !empty($_COOKIE['user'])){
+        echo "Hola ".$_COOKIE['user']."!";
+    }
+    
 ?>
