44ac9a1f8597e3f73396283649944d35db995593
josbancar <banacloyjcarcelen@gmail.com>
2024-10-17 13:32:37 +0200

    v2 (ejercicio no conseguido)

--- ej2/recordar.php
+++ ej2/recordar.php
@@ -1,19 +1,44 @@
 <?php 
-    session_start();
-
+    $nombre = $_POST['nombre'];
+    
     if (isset($_POST['recordar'])){
-        $_SESSION['nombreUser'] = $_POST['nombre'];
-        $_SESSION['passUser'] = $_POST['pass'];
-        setcookie('user', $_SESSION['nombreUser'], time()+1000);
-        echo "Se creo la cookie.";
+        setcookie('user', $nombre , time()+1000);  
+        echo "Se te recordara la proxima vez"; 
+       
+    }
+
+    
+    
+    if (isset($_COOKIE['user']) && !empty($_COOKIE['user'])){
+        echo "Hola ".$_COOKIE['user']."!";
     }
     // else{
     //     unset($_SESSION['nombreUser']);
     //     unset($_SESSION['passUser']);
     //     session_destroy();
-    // }
-    if (isset($_COOKIE['user']) && !empty($_COOKIE['user'])){
-        echo "Hola ".$_COOKIE['user']."!";
-    }
+    // }   
     
+
+
+
+
+    /* Código de mierda de chatgpt */ 
+
+     // Comprobar si ya existe la cookie en una nueva sesión ANTES de cualquier salida
+    // if (isset($_COOKIE['user']) && !empty($_COOKIE['user'])) {
+    // echo "Bienvenido de nuevo, " . htmlspecialchars($_COOKIE['user']) . "!";
+    // } else {
+        // Comprobar si se ha enviado el formulario
+    //     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    //         $nombre = $_POST['nombre'];
+            
+           // Si la opción "Recordar" está seleccionada, crear la cookie
+    //         if (isset($_POST['recordar'])) {
+    //             setcookie('user', $nombre, time() + 3600 * 24 * 30); // La cookie dura 30 días
+    //             echo "Hola $nombre, te recordaremos en tu próxima sesión.";
+    //         } 
+    //     } else {
+    //         echo "Inicia la sesión.";
+    //     }
+    // }
 ?>
