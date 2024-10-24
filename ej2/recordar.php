a741071f83125a000b3d88e3ec508b8caf52e5bc
josbancar <banacloyjcarcelen@gmail.com>
2024-10-24 19:09:21 +0200

    ej calificaciones

--- ej2/recordar.php
+++ ej2/recordar.php
@@ -1,28 +1,23 @@
 <?php 
     //Comprobar si el form se envia.
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $nombre = $_GET['nombre'];
-        $password = $_GET['pass'];
-        session_start();
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $nombre = $_POST['nombre'];
+        $password = $_POST['pass'];
 
-        if (isset($_GET['recordar'])){
+        if (isset($_POST['recordar'])){
             //Si el usuario marca la opción "Recordar", crear la cookie
-            $_SESSION['nombre_usuario'] = $nombre;
-            setcookie('user', $nombre , time()+3600, "/");  
+            setcookie('userEj2', $nombre , time()+3600);  
         }
         else{
             //Borrar la cookie si no marca la opción "Recordar"
-            unset($_SESSION['nombre_usuario']);
-            setcookie('user', $nombre, time()-3600, "/");
+            setcookie('userEj2', $nombre, time()-3600);
         }
     }
-    
+    print_r($_COOKIE);
+    echo "Hola bienvenido " . $_COOKIE['userEj2'];
     //Cromprobar que la cookie existe
-    if (isset($_COOKIE['user'])){
-        echo "Hola bienvenido " . $_SESSION['nombre_usuario'];
-        echo "Hola bienvenido " . $_COOKIE['user'];
-    }else{
-        echo "Inicia sesion primero.";
-    }
+    // if (isset($_COOKIE['user'])){
+        
+    // }
         
 ?>
